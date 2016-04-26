0271d13cb07f80e3ece2cb0fc1b9291f0d0cf2de
Thiago Figueiró <thiagocsf@gmail.com>
2016-04-26 15:58:53 +1000

    Introduce WORKERS_PER_INSTANCE constant

--- lib/aws_helper/constants.rb
+++ lib/aws_helper/constants.rb
@@ -1,13 +1,14 @@
 class AwsHelper
-  PROJECT_TAG = 'tagtrue'
-  RAILS_ENV = ENV['RAILS_ENV'] || 'development'
-  METADATA_URI = URI.parse('http://169.254.169.254/latest/meta-data')
-  WORKER_INSTANCE_LIMIT = 'production'.eql?(RAILS_ENV) ? 10 : 4
-  CONFIG_NAME_BASE = "ttworker_#{RAILS_ENV}_"
-  AMI_NAME_BASE = 'tagtrue_worker_v'
+  PROJECT_TAG             = 'tagtrue'
+  RAILS_ENV               = ENV['RAILS_ENV'] || 'development'
+  METADATA_URI            = URI.parse('http://169.254.169.254/latest/meta-data')
+  WORKER_INSTANCE_LIMIT   = 'production'.eql?(RAILS_ENV) ? 10 : 4
+  WORKERS_PER_INSTANCE    = 'development'.eql?(RAILS_ENV) ? 1 : 4
+  CONFIG_NAME_BASE        = "ttworker_#{RAILS_ENV}_"
+  AMI_NAME_BASE           = 'tagtrue_worker_v'
   # When rebooting instances, wait for this percentage to come back before batch rebooting.
-  REBOOT_WAIT_COUNT_PCT = 0.5
-  REBOOT_TIMEOUT = 60 # seconds
-  INSTANCE_STARTUP_TIME = 250 # seconds - measured at 220s so add a bit of room.
-  AWS_OBJECT_CLEANUP_AGE = 30 # days
+  REBOOT_WAIT_COUNT_PCT   = 0.5
+  REBOOT_TIMEOUT          = 60 # seconds
+  INSTANCE_STARTUP_TIME   = 250 # seconds - measured at 220s so add a bit of room.
+  AWS_OBJECT_CLEANUP_AGE  = 30 # days
 end
